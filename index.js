fc43eaed17b054c1b4cd85fc58f8543603150415
Ujj <ujjwal@messagebus.com>
2014-07-22 15:23:21 -0700

    adding rotating-file

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
 var log = bunyan.createLogger({
     name: options.logName || 'default',
     streams: [{
-        level: 'info',
-        stream: options.outLogFile
-    }, {
-        level: 'error',
-        path: options.outLogFile
+        type: 'rotating-file',
+        path: options.outLogFile,
+        period: '1d',
+        count: 7
     }]
 });
 
